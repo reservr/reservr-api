9736e5245b4c57dd4d4e6512da5dd1875216ce5d
Sebi Kovacs <sebi.kovacs@gmail.com>
2018-03-16 18:00:39 +0200

    Added eslint

--- server.js
+++ server.js
@@ -21,44 +21,45 @@ app.all( "*", function( req, res, next ) {
     next();
 } );
 
-var Datastore = require( "nedb" );
-var db = {};
+const Datastore = require( "nedb" );
+
+const db = {};
 
 const config = {
     dataDir: "data",
     dbDir: "/db",
     ipAddress: "127.0.0.1",
     port: 8080
-}
+};
 
 db.events = new Datastore( {
-    filename: config.dataDir + config.dbDir + "/events.db",
+    filename: `${ config.dataDir + config.dbDir }/events.db`,
     autoload: true
 } );
 
 db.users = new Datastore( {
-    filename: config.dataDir + config.dbDir + "/users.db",
+    filename: `${ config.dataDir + config.dbDir }/users.db`,
     autoload: true
 } );
 
 db.orgs = new Datastore( {
-    filename: config.dataDir + config.dbDir + "/orgs.db",
+    filename: `${ config.dataDir + config.dbDir }/orgs.db`,
     autoload: true
 } );
 
 db.reservations = new Datastore( {
-    filename: config.dataDir + config.dbDir + "/reservations.db",
+    filename: `${ config.dataDir + config.dbDir }/reservations.db`,
     autoload: true
 } );
 
 db.mcapikeys = new Datastore( {
-    filename: config.dataDir + config.dbDir + "/mcapikeys.db",
+    filename: `${ config.dataDir + config.dbDir }/mcapikeys.db`,
     autoload: true
 } );
 
 app.get( "/orgs", ( req, res ) => {
     db.orgs.find( {}, ( err, orgs ) => {
-        res.send( { orgs } )
+        res.send( { orgs } );
     } );
 } );
 
@@ -67,9 +68,10 @@ app.get( "/events/:slug", ( req, res ) => {
     const lastThirtyDays = new Date( new Date().getTime() - daysInMiliseconds( 30 ) );
 
     db.orgs.findOne( { slug: slug.toLowerCase() }, ( err, org ) => {
-        db.events.find( { orgId: org._id, date: {
-            $gt: lastThirtyDays
-        } }, ( err, events ) => {
+        db.events.find( { orgId: org._id,
+            date: {
+                $gt: lastThirtyDays
+            } }, ( secondErr, events ) => {
             res.send( { events } );
         } );
     } );
@@ -83,7 +85,6 @@ app.listen( config.port, config.ipAddress, function() {
         config.port
     );
 } );
-
 
 function daysInMiliseconds( days = 1 ) {
     return days * 8.64e+7;
